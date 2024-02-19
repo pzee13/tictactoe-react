a3f90323b9a8785223cee3c66f8a42df27d07984
ASWIN P C <aswinpc9@gmail.com>
2024-02-19 12:14:14 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import Tictac from './Tictac'
+function App() {
+
+
+  return (
+    <>
+    <div className=''>
+          <img src='/src/assets/icons8-tic-tac-toe-32.png' className="logo react" alt="React logo" />
+      <Tictac />
+      </div>
+    </>
+  )
+}
+
+export default App
